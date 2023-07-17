9641c7791dda4c1e37bfb264298cf23c1fdf1cb4
Sindhuja-Mani <139770670+Sindhuja-Mani@users.noreply.github.com>
2023-07-17 17:45:54 +0530

    Add files via upload
    Day 1 Assignments and Examples

---
+++ day1/Car.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int price = 1200000;
+String brandName="Hyundai";
+String fuelCapacity="12.45lt";
+char logo='H';
+float fuel=12.45f;
+double modelVarient=456.23423434;
+boolean isNew=false;
+long contactNumber=98799897877l;
+System.out.println(price);
+System.out.println(brandName);
+System.out.println(fuelCapacity);
+System.out.println("The logo of the car" + logo);
+System.out.println(fuel);
+System.out.println(modelVarient);
+System.out.println(isNew);
+System.out.println(contactNumber);
+System.out.println(contactNumber+": "+price);
+	}
+
+}
