d2da34e8ec30f393a173fabf7169dd2562e8c3e3
Sindhuja-Mani <139770670+Sindhuja-Mani@users.noreply.github.com>
2023-07-19 09:50:52 +0530

    Update FibonacciSeries.java

--- day2/FibonacciSeries.java
+++ day2/FibonacciSeries.java
@@ -10,17 +10,14 @@ int secNum = 1;
 int sum;
 System.out.println(firstNum);
 System.out.println(secNum);
-for(int i=1;i<range;i++)
+for(int i=0;i<range;i++)
 {
 
 	sum=firstNum+secNum;
 	firstNum=secNum;
 	secNum=sum;
 	System.out.println(sum);
-	if(sum==13)
-	{
-	break;	
-	}
+	
 }
 
 	}
